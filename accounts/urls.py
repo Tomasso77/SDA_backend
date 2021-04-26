925302e7c8e5c0fddd94db0a3b10660b642c364f
tomasz <tomasz.falat@falat.pl>
2021-04-26 19:03:40 +0200

    User crate form (/accounts/signup/)

--- accounts/urls.py
+++ accounts/urls.py
@@ -19,11 +19,13 @@ from django.urls import path, include
 
 from accounts.views import (
                           SubmittableLoginView,
-                          SubmittablePasswordChangeView
+                          SubmittablePasswordChangeView,
+                          signup,
                           )
 app_name = "accounts"
 
 urlpatterns = [
+    path('signup/', signup, name="signup"),
     path('login/', SubmittableLoginView.as_view(), name="login"),
     path('logout/', LogoutView.as_view(), name="logout"),
     path('password-change/', SubmittablePasswordChangeView.as_view(), name="password_change")
